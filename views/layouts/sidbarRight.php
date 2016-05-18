4764385a1fd39201b6d5eafa41bd58ba93bf42c0
kamgroupboris <kamgroupboris@yandex.ru>
2016-05-18 17:06:19 +0300

    Правое меню

---
+++ views/layouts/sidbarRight.php
@@ -0,0 +1,28 @@
+﻿	<?
+	use yii\bootstrap\Nav;
+	use yii\bootstrap\NavBar;
+	use app\models\Menu;
+	?>
+
+<aside class="col-xs-3 sidbar_gl ">
+		<div class="sidbarRight">
+			<?
+
+			$footM = Menu::find()->select('label,url')->where(['type_menu'=>'right','active'=>2])->asArray()->all();
+
+
+			echo Nav::widget([
+					'options' => ['class' => 'footMenu', 'id'=>'footM'],
+					'items' => $footM,
+			]);
+
+			?>
+		</div>
+		<div class="asideRightImg">
+			<ul>
+				<li><a href="#"><img src="img/asideRightImg.png" alt=""></a></li>
+				<li><a href="#"><img src="img/asideRightImg.png" alt=""></a></li>
+				<li><a href="#"><img src="img/asideRightImg.png" alt=""></a></li>
+			</ul>
+		</div>
+	</aside> 
